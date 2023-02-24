9de9247a198d3cb695e056dfff2c6eb300f0a21a
Pierro236 <93871665+Pierro236@users.noreply.github.com>
2023-02-24 21:22:34 +0100

    update : tour.js

--- tour.js
+++ tour.js
@@ -1,6 +1,6 @@
 const lesBlacks = document.getElementsByClassName('black');
 
-//MYSQL_PASSWORD = ygyzedg68hgd
+MYSQL_PASSWORD = ygyzedg68hgd
 
 //TODO: Utiliser Event.stopPropagation pour gérer les tours (blocker les blancs quand c'est au tour des noir )
 // https://stackoverflow.com/questions/5241981/disable-mouse-double-click-using-javascript-or-jquery
